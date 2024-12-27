bac3e9644833aa188644e5b5dff0662a4c170002
Alex De Luca <alexnaj88@gmail.com>
2024-12-26 19:27:42 -0500

    initial search bar dyanmic sizing

--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from "react"
 import styles from "../../../styles/search.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import SearchBarHeading from "./SearchBarHeading"
 
 export default function SearchBar(): React.JSX.Element {
@@ -11,14 +13,24 @@ export default function SearchBar(): React.JSX.Element {
         setTimeout(() => setVisible(true), 400)
     }, [])
 
+    function weatherSearch() {
+        console.log("Searching...")
+    }
+
     return(
         <div className={visible ? `${styles.weatherSearch} ${styles.visible}` : styles.weatherSearch}>
             <SearchBarHeading />
-            <input
-                className={styles.searchBar}
-                type="text"
-                name="weatherLocation"
-                placeholder="Search a City or Zip Code" />
+            <div className={styles.searchBarDiv}>
+                {/* Magnifying glass */}
+                <form className={styles.searchForm} action="/search">
+                    <FontAwesomeIcon icon={fas.faMagnifyingGlass} onClick={() => weatherSearch()} />
+                    <input
+                        className={styles.searchBar}
+                        type="text"
+                        name="weatherLocation"
+                        placeholder="Search a City or Zip Code..." />
+                </form>
+            </div>
         </div>
     )
 }
